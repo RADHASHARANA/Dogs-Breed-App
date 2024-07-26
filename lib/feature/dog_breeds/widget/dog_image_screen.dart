6e9d347085473dc8891ee3f29c5f0f4a7810f4fd
Radhasharan <radhasharan.pradhan@5Paisa.com>
2024-07-26 11:48:22 +0530

    Image Scaling

--- lib/feature/dog_breeds/widget/dog_image_screen.dart
+++ lib/feature/dog_breeds/widget/dog_image_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_networking/feature/common_widgets/status_handler.dart';
 import 'package:flutter_networking/feature/dog_breeds/bloc/dog_breeds_bloc.dart';
 import 'package:flutter_networking/feature/dog_breeds/bloc/dog_breeds_state.dart';
+import 'package:flutter_networking/utils/strings_utils.dart';
 
 class DogImageScreen extends StatelessWidget {
   const DogImageScreen({super.key, required this.breedName});
@@ -12,7 +13,7 @@ class DogImageScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('$breedName Image'),
+        title: Text('${breedName.capitalizeFirstChar()} Image'),
       ),
       body: BlocBuilder<DogBreedsCubit, DogBreedsState>(
         buildWhen: (previous, current) =>
@@ -20,12 +21,37 @@ class DogImageScreen extends StatelessWidget {
         builder: (context, state) {
           return StatusHandler(
             status: state.dogImageStatus,
-            onSuccess: Center(
-              child: Image.network(
-                state.breedImageUrl,
-                fit: BoxFit.contain,
-                width: double.infinity,
-                height: double.infinity,
+            onSuccess: SafeArea(
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.grey.shade900, // Darker gray
+                          Colors.grey.shade700, // Lighter gray
+                        ],
+                      ),
+                    ),
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: InteractiveViewer(
+                      maxScale: 3.0,
+                      minScale: 0.5,
+                      child: Image.network(
+                        state.breedImageUrl,
+                        fit: BoxFit.contain,
+                        width: double.infinity,
+                        height: double.infinity,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
             error: state.subbreedListerror,
